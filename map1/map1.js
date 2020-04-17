6f94759795b88c27c1a2efaaa7b015343e8e4bd2
mds32696 <63761297+mds32696@users.noreply.github.com>
2020-04-17 07:56:35 -0500

    Update map1.js

--- map1/map1.js
+++ map1/map1.js
@@ -1,13 +1,13 @@
-var Hastings = L.map('Hastings Battlefield Map').setView([50.914750, 0.488450], 6)
-L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png').addTo('Hastings Battlefield Map')
-var 'Battle Site' = L.marker([50.914750, 0.488450]).addTo('Hastings Battlefield Map')
+var Hastings = L.map(map1).setView([50.914750, 0.488450], 6)
+L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png').addTo(map1)
+var 'Battle Site' = L.marker([50.914750, 0.488450]).addTo(map1)
 var 'Norman Invasion Route' = L.polygon([
   [50.859280, 0.600819],
   [50.815250, 0.353041],
   [51.499286, -0.125230]
-]).addTo('Hastings Battlefield Map')
+]).addTo(map1)
 'Norman Invasion Route'.bindPopup('This polygon marks the route the Norman army took to the battlefield at Senlac Hill, near Hastings, during their invasion of England in October 1066.')
 'Senlac Hill Battlefield'.bindPopup('This marker denotes the approximate location of the battlefield at Senlac Hill. The defending Anglo-Saxon army under the command of King Harold Godwinson held the steep hill to the north, while the Norman army under the command of Duke William assaulted from the south.')
 var latlngs = [[54.000376, -0.805329], [52.434269, -1.946722], [51.459164, -0.133978]];
 var polyline = L.polyline(latlngs, {color: 'red'});
-]).addTo('Hastings Battlefield Map')
+]).addTo(map1)
