62dd7d991e43f5d39d6e515d684445c5fd54add3
mds32696 <63761297+mds32696@users.noreply.github.com>
2020-04-18 04:47:31 -0500

    Add files via upload

---
+++ newmap2/newmap2.js
@@ -0,0 +1,14 @@
+var newmap2 = L.map('newmap2').setView([39.112974, -94.671063], 4)
+L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png').addTo(newmap2)
+L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
+  layers: 'nexrad-n0r-900913',
+  format: 'image/png',
+  transparent: true,
+  attribution: 'NOAA, Iowa State University'
+}).addTo(newmap2)
+L.tileLayer.wms('https://mesonet.agron.iastate.edu/cgi-bin/wms/us/wwa.cgi', {
+  layers: 'warnings_c',
+  format: 'image/png',
+  transparent: true,
+  attribution: 'NOAA, Iowa State University'
+}).addTo(newmap2)
