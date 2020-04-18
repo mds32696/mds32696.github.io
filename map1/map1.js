728b740b40a43c989c9fceafeade3d9230143246
mds32696 <63761297+mds32696@users.noreply.github.com>
2020-04-17 22:55:34 -0500

    Update map1.js

--- map1/map1.js
+++ map1/map1.js
@@ -7,7 +7,7 @@ var 'Norman Invasion Route' = L.polygon([
   [51.499286, -0.125230]
 ]).addTo(map1)
 'Norman Invasion Route'.bindPopup('This polygon marks the route the Norman army took to the battlefield at Senlac Hill, near Hastings, during their invasion of England in October 1066.')
-'Senlac Hill Battlefield'.bindPopup('This marker denotes the approximate location of the battlefield at Senlac Hill. The defending Anglo-Saxon army under the command of King Harold Godwinson held the steep hill to the north, while the Norman army under the command of Duke William assaulted from the south.')
+'Battle Site'.bindPopup('This marker denotes the approximate location of the battlefield at Senlac Hill. The defending Anglo-Saxon army under the command of King Harold Godwinson held the steep hill to the north, while the Norman army under the command of Duke William assaulted from the south.')
 var latlngs = [[54.000376, -0.805329], [52.434269, -1.946722], [51.459164, -0.133978]];
 var polyline = L.polyline(latlngs, {color: 'red'});
 ]).addTo(map1)
